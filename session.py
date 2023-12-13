6df8514059841978f8d3d34e0d81b428c62e65e3
jhickmanarc <jhickman@arcdigitalservices.com>
2023-12-12 22:05:20 -0800

    Initial migration of core authentication functions

---
+++ session.py
@@ -0,0 +1,63 @@
+from chordata.server_env import ServerEnvironment
+from chordata.util.tenant_prefix import tenant_prefix
+
+
+def login(e: ServerEnvironment, s: dict):
+    prefix = tenant_prefix(e)
+    injector = e.get_injection_manager()
+    AuthenticationModel = injector.get('apps.authentication.lib.m.authenticate', 'AuthenticationModel')
+    AuthenticateController = injector.get('apps.authentication.lib.c.authenticate', 'AuthenticateController')
+    controller = AuthenticateController(
+        s,
+        e.get_configuration(),
+        [
+            AuthenticationModel(
+                e.get_database(),
+                {
+                    **e.get_configuration(),
+                    **{'tenant': e.get_tenant()}
+                }
+            )
+        ]
+    )
+    r = {
+        "authenticated": False
+    }
+    mode = e.get_data_type()
+    if mode == "POST" or mode == "JSON":
+        data = e.get_data().getValues()
+        values = {
+            'username': data.get('username', {}).get('value'),
+            'password': data.get('password', {}).get('value'),
+            'tenant': e.get_tenant()
+        }
+        r = controller.do_post(values)
+        if not r['authenticated']:
+            if mode == "POST":
+                r['failed_login'] = True
+                r['forward_to'] = e.query.get('forward_to', '')
+                forward_to = e.query.get('forward_to', '/')
+                return {}, {"redirect": prefix + "/authentication/login?failed=1&forward_to=" + forward_to}
+            else:
+                return r, {'serviceOf': 'json'}
+        else:
+            e.get_event_manager().send("user_authenticated", r)
+    if mode == "JSON":
+        return [r, {'serviceOf': 'json'}]
+    else:
+        forward_to = e.query.get('forward_to', '/')
+        return {}, {"redirect": forward_to}
+
+
+def end_session(e: ServerEnvironment, s: dict):
+    keys = s.keys()
+    for key in list(keys):
+        del s[key]
+    return {}, {'serviceOf': 'json'}
+
+
+def logout(e: ServerEnvironment, s: dict):
+    keys = s.keys()
+    for key in list(keys):
+        del s[key]
+    return {}, {"redirect": tenant_prefix(e) + "/authentication/login"}
---
+++ setup.py
@@ -0,0 +1,67 @@
+def schema():
+    return "authentication"
+
+
+def setup(database):
+    from sqlalchemy import Table, Column, ForeignKey, MetaData, Integer, String, Boolean, DateTime
+    database.create_schema(schema())
+    engine = database.get_engine()
+    meta = MetaData(schema='authentication')
+
+    Table(
+        'user', meta,
+        Column('id', Integer, primary_key=True, autoincrement=True, unique=True),
+        Column('user_name', String(128)),
+        Column('email', String(128)),
+        Column('password_hash', String(256)),
+        Column('password_salt', String(128)),
+        Column('dav_password', String(128)),
+        Column('local_login', Boolean()),
+        Column('first_name', String(128)),
+        Column('last_name', String(128)),
+        Column('phone', String(128)),
+        Column('organization', String(128)),
+        Column('active', Boolean()),
+        Column('created', DateTime()),
+        Column('last_login', DateTime()),
+        Column('reset_hash', String(128)),
+        Column('reset_timestamp', DateTime())
+    )
+
+    Table(
+        'roles', meta,
+        Column('id', Integer, primary_key=True, autoincrement=True, unique=True),
+        Column('name', String(32))
+    )
+
+    Table(
+        'users_roles', meta,
+        Column('user', ForeignKey("user.id")),
+        Column('role', ForeignKey("roles.id"))
+    )
+
+    Table(
+        'oauth', meta,
+        Column('provider', String(128), primary_key=True),
+        Column('client_id', String(255)),
+        Column('secret', String(255)),
+        Column('redirect', String(255))
+    )
+
+    meta.create_all(engine, checkfirst=True)
+    init_roles(database)
+    return
+
+
+def init_roles(database):
+    from apps.authentication.lib.models.roles import Roles
+    sess = database.get_connection()
+    roles = ['user', "owner", "manager"]
+    for role in roles:
+        existing = sess.query(Roles).filter(Roles.name == role).first()
+        if existing is None:
+            new_role = Roles(name=role)
+            sess.add(new_role)
+            sess.commit()
+    sess.close()
+    return
